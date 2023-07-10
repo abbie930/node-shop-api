631fdf01fead45bba372fe4aa08c6f9feefd7e1c
abbie930 <abbiecheng930@gmail.com>
2023-07-10 22:14:00 +0800

    feat: set product router and  add create product function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
 // mongoose connect
 require('./config/mongoose')
 
@@ -16,7 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
-
+app.use('/api/products', productRoute)
 
 
 app.listen(PORT, () => {
