f00030c3a34cf347889342b117da39da391df9b8
abbie930 <abbiecheng930@gmail.com>
2023-07-10 20:19:40 +0800

    feat: add Admin find user function

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const User = require('../models/User')
-const { verifyToken, verifyTokenAndAuthorization } = require('../middleware/verifyToken')
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('../middleware/verifyToken')
 
 
 // EDIT
@@ -38,5 +38,18 @@ router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json({ error: err.message })
   }
 })
+
+// GET USER
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const { password, ...others } = user._doc
+    res.status(200).json(others)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+
 
 module.exports = router
