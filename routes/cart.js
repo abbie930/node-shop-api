40f8f604bd215127f6149eece17e31a007da8fd4
abbie930 <abbiecheng930@gmail.com>
2023-07-10 22:37:32 +0800

    feat: add edit cart function

--- routes/cart.js
+++ routes/cart.js
@@ -14,4 +14,20 @@ router.post('/', verifyToken, async (req, res) => {
   }
 })
 
+// EDIT (user can only change its own card )
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body
+      },
+      { new: true }
+    )
+    res.status(200).json(updatedCart)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router
