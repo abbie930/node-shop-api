c04054d8096b84001af12e6a19a533cf05fbff0d
abbie930 <abbiecheng930@gmail.com>
2023-07-10 22:58:38 +0800

    feat: add get users orders function

--- routes/order..js
+++ routes/order..js
@@ -39,3 +39,14 @@ router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
     res.status(500).json(err)
   }
 })
+
+//GET USER ORDERS
+router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    // users can have more than one orders
+    const orders = await Order.find({ userId: req.params.userId })
+    res.status(200).json(orders)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
